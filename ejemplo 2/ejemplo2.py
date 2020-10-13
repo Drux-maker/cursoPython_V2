dd7d3472b41c5184f04d473b3f0f046e645f7921
unknown <danieljimenezdearriba@gmail.com>
2020-10-13 11:30:00 +0200

    C:\Users\yo\Desktop\programacion\cursoPython_V2\ejemplo 2

---
+++ ejemplo 2/ejemplo2.py
@@ -0,0 +1,29 @@
+
+
+array_num = list(range(0,101))
+array_nombre = ['d','a','n','i','e','l','j','i','m','e','n','e','z','d','e','a','r','r','i','b','a']
+x = 0
+#recorro el array y lo imprimo 
+"""
+for x in array_num:
+    print(x)
+
+
+for x in range(10):
+    print(array_num[x])
+    print('la letra',x, 'es', array_nombre[x+1])
+    if
+    ç
+
+   
+for x in range(0,101,2):
+    print(array_num[x])
+
+while x<1:
+    print (array_num)
+    x = x+1
+
+for x in array_num:
+    if(array_num[x]%3>0):
+        print(array_num[x])
+"""
