4d77bf4825fd016d5ab696bd0b7d831237140d1b
unknown <danieljimenezdearriba@gmail.com>
2020-10-15 13:16:13 +0200

    comentario

---
+++ ejemplo 3/ejemplo3.py
@@ -0,0 +1,23 @@
+import random
+import time
+
+numeros = (random.randint(0,10))
+
+acierto = False
+
+    
+print("escribe tu numero del 1 al 10")
+a = input
+   
+
+while (not acierto):
+    if a == numeros:
+        print ("acertaste")
+        acierto = True
+        break
+
+            
+        
+        
+    
+    
