f54951e433119bb64a43ac4ab621ef744e27587b
unknown <danieljimenezdearriba@gmail.com>
2020-11-03 12:29:51 +0100

    ejemplo6

--- ejemplo 6/ejemplo 6.py
+++ ejemplo 6/ejemplo 6.py
@@ -1,10 +1,12 @@
 #prueba 6
 
-
+#creo las variables
 lista1 = []
 lista2 = []
 lista3 = []
 lista4 = []
+contador = 0
+contador1 = 0
 
 #creo el array de 100 numeros
 for x in range (101):
@@ -13,12 +15,20 @@ for x in range (101):
 for x in range (101):
     if (lista1[x]%2 == 0):
         lista2.append(lista1[x])
-        
-for x in range (101):
+        contador = contador+1
+
+for x in range (contador):
     if (lista2[x]%4 == 0):
         lista3.append(lista2[x])
-
-for x in range (101):
+        contador1 = contador1+1
+      
+for x in range (contador1):
     if (lista3[x]%5 == 0):
-        lista4.
-print (lista4)
+        lista3[x]= "*"
+ 
+for x in range (contador):
+    if (lista4[x]%2 == 1):
+        lista3.append(x)
+
+print (lista3)
+
